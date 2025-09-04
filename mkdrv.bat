72b8c614b24483fa46d286924d86da0530f51b62
Chad Doebelin <chad.doebelin@gmail.com>
2025-09-04 00:13:52 -0500

    Add DLL entry stubs

--- mkdrv.bat
+++ mkdrv.bat
@@ -1,3 +1,3 @@
 rem Link the Tandy 16-color driver using Windows 3.0 libraries
 rem Include LIBENTRY to route startup through LibMain/WEP instead of WinMain
-link libentry.obj enable.obj tgavid.obj, tndy16.drv,, libw slibcew gdi kernel user, tndy16.def
+link libentry.obj dllentry.obj enable.obj tgavid.obj, tndy16.drv,, libw slibcew gdi kernel user, tndy16.def
