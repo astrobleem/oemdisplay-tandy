3df46396bbcc700cda4a9a6a658b7641edad4d85
Chad Doebelin <chad.doebelin@gmail.com>
2025-09-03 23:19:52 -0500

    Use LibEntry startup for driver link

--- mkdrv.bat
+++ mkdrv.bat
@@ -1,2 +1,3 @@
 rem Link the Tandy 16-color driver using Windows 3.0 libraries
-link enable.obj tgavid.obj, tndy16.drv,, libw slibcew gdi kernel user, tndy16.def 
+rem Include LIBENTRY to route startup through LibMain/WEP instead of WinMain
+link libentry.obj enable.obj tgavid.obj, tndy16.drv,, libw slibcew gdi kernel user, tndy16.def
